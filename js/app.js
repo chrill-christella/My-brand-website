b8ca92b9f820395a762e7da948ec89e8f7a6cc10
chrill-christella <ufiteyezuchristella@gmail.com>
2022-12-10 23:28:14 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 450) {
+    header.style.backgroundColor = "#384A49";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
+
+//dashboard
+let menu = document.querySelector(".menu");
+let sidebar = document.querySelector(".sidebar");
+let mainContent = document.querySelector(".main--content");
+
+menu.onclick = function () {
+  sidebar.classList.toggle("active");
+  mainContent.classList.toggle("active");
+};
---
+++ js/swiper-script.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
